033b261ffc028c20cddadee7a97c16f78ac8822b
Desmond Gukwe <gukwedesmond@gmail.com>
2020-03-04 11:48:32 +0200

    Backward elemenation

--- Machine Learning A-Z New/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Machine Learning A-Z New/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -39,3 +39,25 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+#Building n optimal model using backward selection , must remove the variable with p value >0.05
+import statsmodels.api as sm
+X = np.append(arr=np.ones((50,1)).astype(int),values =X,axis=1)
+X_opt=X[:,[0,1,2,3,4,5]]
+regressor_OLS =sm.OLS(endog=y,exog =X_opt).fit()
+regressor_OLS.summary()
+##Remove one variable
+
+X_opt=X[:,[0,1,3,4,5]]
+regressor_OLS =sm.OLS(endog=y,exog =X_opt).fit()
+regressor_OLS.summary()
+
+X_opt=X[:,[0,3,4,5]]
+regressor_OLS =sm.OLS(endog=y,exog =X_opt).fit()
+regressor_OLS.summary()
+
+
+
+X_opt=X[:,[0,3,5]]
+regressor_OLS =sm.OLS(endog=y,exog =X_opt).fit()
+regressor_OLS.summary()
