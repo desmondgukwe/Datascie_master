6d508afab1e1f680d4d50c1e7ccdbae06bb5ad6f
Desmond Gukwe <gukwedesmond@gmail.com>
2020-03-04 14:22:14 +0200

    Auto backwardElimination

--- Machine Learning A-Z New/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
+++ Machine Learning A-Z New/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
@@ -1,7 +1,7 @@
 # Multiple Linear Regression
 
 # Importing the dataset
-dataset = read.csv('50_Startups.csv')
+dataset = read.csv('~/Datascie_master/Machine Learning A-Z New/Part 2 - Regression/Section 5 - Multiple Linear Regression/50_Startups.csv')
 
 # Encoding categorical data
 dataset$State = factor(dataset$State,
@@ -22,7 +22,30 @@ test_set = subset(dataset, split == FALSE)
 
 # Fitting Multiple Linear Regression to the Training set
 regressor = lm(formula = Profit ~ .,
+               data = dataset)
+summary(regressor)
+## BAckward selection the variable with 3 stars are the most important
+regressor = lm(formula = Profit ~ R.D.Spend,
                data = training_set)
-
-# Predicting the Test set results
+summary(regressor)
+ # Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
+
+#Automated backwardelmination
+backwardElimination <- function(x, sl) {
+  numVars = length(x)
+  for (i in c(1:numVars)){
+    regressor = lm(formula = Profit ~ ., data = x)
+    maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+      x = x[, -j]
+    }
+    numVars = numVars - 1
+  }
+  return(summary(regressor))
+}
+
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
