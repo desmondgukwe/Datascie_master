86addda08611e3700407ae9a27c1307e55e589f4
Desmond Gukwe <gukwedesmond@gmail.com>
2020-03-10 11:17:07 +0200

    Polynomial Py

--- Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
+++ Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('~/Datascie_master/Machine Learning A-Z New/Part 2 - Regression/Section 6 - Polynomial Regression/Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
@@ -19,6 +19,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
+
 
 # Fitting Linear Regression to the dataset
 from sklearn.linear_model import LinearRegression
